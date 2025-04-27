735f9891f96e4cedc2d9fc0aaafd8ff2ec008233
user <user@example.com>
2025-04-28 05:31:16 +0800

    about

--- pages/about.py
+++ pages/about.py
@@ -3,4 +3,13 @@ destination = "public/about/index.html"
 title = "About"
 styles = []
 scripts = []
-content = ""
+content = """
+<div class="card">
+<h2>Name</h2>
+<p>
+Qatoqat is the author's personal brand/trademark.  
+The name `qatoqat` is derived from a wordplay on the Spanish word for cat, `gato`, then suffixed with `-oh cat`, with some letters swapped with `q`.  
+The word `qatoqat` may also mean <a href="https://youtu.be/O9ak89FwYeI?feature=shared&t=70">cat cat</a>.  
+The author may also occasionally use the shortened form, `Qato`.
+</p>
+"""
